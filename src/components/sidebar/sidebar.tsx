bc145bb6d1bb0d880ff3458b1d3072dc6aa192f6
Akojede70 <peter.oluwamayowa.akojede@gmail.com>
2024-09-26 03:10:09 +0100

    work on sidebar responsivenesss

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -8,19 +8,19 @@ function SideBar(): JSX.Element {
 
   return (
     <div className="w-[17.5%] h-screen flex-shrink-0 bg-[#242424]">
-      <div className="pt-20 pl-5 flex flex-col gap-2.5">
+      <div className="pt-20 md:pt-32 lg:pt-20 lg:pl-5 flex flex-col gap-4 md:gap-2.5">
         {paths.map((x) => (
           <div key={x.id}>
             <div
-              className={`flex items-center h-8 pl-[10%] cursor-pointer text-capitalize w-[75%] hover:bg-[#5A5A5A] hover:text-[#ffffff]
-                ${window.location.pathname === x.path ? 'bg-[#3B3B3B] h-10 w-75%' : 'text-[#7E7F7F]'}`}
+              className={`flex items-center h-8 lg:pl-[10%] cursor-pointer text-capitalize w-full  lg:w-[75%] hover:bg-[#5A5A5A] hover:text-[#ffffff]
+                ${window.location.pathname === x.path ? 'bg-[#3B3B3B] h-10 md:w-75%' : 'text-[#7E7F7F]'}`}
               onClick={() => x.path && navigate(x.path)}
             >
-              <div className="mr-2">
+              <div className="hidden md:block lg:mr-2">
                 {x.icon}
               </div>
               <p
-    className={`text-base font-normal leading-5 hover:text-[#fffff]  ${
+    className={`text-xs md:text-base font-normal leading-5 hover:text-[#fffff]  ${
       window.location.pathname === x.path ? 'text-[#ffffff]' : 'text-[#7E7F7F]'
     }`}
   >
@@ -30,7 +30,7 @@ function SideBar(): JSX.Element {
             
           </div>
         ))}
-        <div className='mt-[80%] w-[77%] cursor-pointer hover:scale-110 transition-transform duration-300'>
+        <div className='mt-[80%] pl-1 md:pl-3 w-[90%] md:w-[77%] cursor-pointer hover:scale-110 transition-transform duration-300'>
     <img src={Logout} alt='logout'/>
   </div>
       </div>
